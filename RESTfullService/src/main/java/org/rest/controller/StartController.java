869c29b4df45270228a8dea3d609a5f8a9f86962
Ivan <kushnirenkoivan@gmail.com>
2015-12-23 23:03:59 +0200

    server_dontworks(

--- RESTfullService/src/main/java/org/rest/controller/StartController.java
+++ RESTfullService/src/main/java/org/rest/controller/StartController.java
@@ -1,22 +1,16 @@
 package org.rest.controller;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
-
-    @Autowired
-    private ObjectMapper objectMapper;
+public class StartController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String main() {
+    public String getStartPage() {
         return "index";
     }
-
 
 }
