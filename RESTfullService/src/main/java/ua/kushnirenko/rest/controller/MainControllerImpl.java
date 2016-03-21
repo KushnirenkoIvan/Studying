298d5a71266a7313267a14a504c0ac45dfbd59df
Ivan Kushnirenko <kushnirenkoivan@gmail.com>
2016-03-21 12:03:48 +0200

    Setting up logging

--- RESTfullService/src/main/java/ua/kushnirenko/rest/controller/MainControllerImpl.java
+++ RESTfullService/src/main/java/ua/kushnirenko/rest/controller/MainControllerImpl.java
@@ -1,6 +1,7 @@
 package ua.kushnirenko.rest.controller;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,18 +11,25 @@ import ua.kushnirenko.entity.User;
 @RestController
 public class MainControllerImpl implements MainController {
 
-    private final Logger log = Logger.getLogger(MainController.class);
+    @Autowired
+    private Logger log;
+
 
 
     @RequestMapping(value = "getUser", method = RequestMethod.GET)
     @Override
     public User restGet() {
+
+        log.info("GET method was invoked.");
+
         return new User(1234, "user", 23);
     }
 
     @RequestMapping(value = "putData", method = RequestMethod.PUT)
     @Override
     public ResponseEntity restPut(@RequestBody User user) {
+
+        log.info("PUT method was invoked.");
 
         ResponseEntity response = null;
 
@@ -38,6 +46,8 @@ public class MainControllerImpl implements MainController {
     @RequestMapping(value = "postData", method = RequestMethod.POST)
     @Override
     public ResponseEntity restPost(@RequestBody User user) {
+
+        log.info("POST method was invoked.");
 
         ResponseEntity response = null;
 
@@ -56,6 +66,8 @@ public class MainControllerImpl implements MainController {
     @Override
     public ResponseEntity restDeleted(@PathVariable String data) {
         ResponseEntity response = null;
+
+        log.info("DELETE method was invoked.");
 
         if (data == null) {
             log.warn("Received data is null.");
